65b0870b91b4656ad929691379f9533e572dbfe3
UmairKhankhail <59060383+UmairKhankhail@users.noreply.github.com>
2021-10-10 23:46:39 +0500

    magic of assignment operator

---
+++ if esle statement.c
@@ -0,0 +1,10 @@
+main()
+{
+    int x=5;
+
+    if (x=4)  // here assignment operator is used and x contain a non-zero digit which is always true
+        printf("Hello %d",x);
+    else
+        printf("Else statement has executed.");
+    getch();
+}
